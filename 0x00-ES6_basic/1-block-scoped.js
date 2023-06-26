feee318b7658fa3ac99e84839bb7bad1781a38ac
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 <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>
2023-06-27 01:57:13 +0300

    comm

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,11 @@ export default function taskBlock(trueorFalse) {
     const task2 = true;
 
     if (trueorFalse) {
+        // eslint-disable-next-line no-unused-vars, no-shadow
         const task = true;
+        // eslint-disable-next-line no-unused-vars, no-shadow
         const task2 = false;
     }
 
     return [task, task2];
 }
-
