8685900b8c0db825d8b4b2b53dabc41362883b07
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 <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>
2023-06-28 00:48:48 +0300

    overwritten

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,15 @@
 export default function taskBlock(trueorFalse) {
     const task = false;
+    //that the variables aren’t overwritten inside the conditional block.
     const task2 = true;
+    //that the variables aren’t overwritten inside the conditional block.
 
     if (trueorFalse) {
         const task = true;
+        //that the variables aren’t overwritten inside the conditional block.
         const task2 = false;
+        //that the variables aren’t overwritten inside the conditional block.
     }
 
     return [task, task2];
 }
-
